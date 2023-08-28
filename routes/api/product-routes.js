2118a3e896f53fd1d87e0c8be528368b4a291ab3
BenWhann <whann004@umn.edu>
2023-08-28 17:46:14 -0500

    all routes working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -54,7 +54,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body?.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -69,7 +69,7 @@ router.post('/', (req, res) => {
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(400).json(err.message);
     });
 });
 
